beefd0676f7eb6a828bbafd0f569dccd2d7bfcd6
20231212-Zhan99 <20231212-Zhan99@foo.com>
2024-03-05 17:41:32 +0800

    update

--- 1.py
+++ 1.py
@@ -1,1 +1,14 @@
-print(123)
+import os
+import subprocess
+
+
+class _Helper:
+    @staticmethod
+    def _linux_cmd(cmd: str):
+        process = subprocess.Popen(cmd, shell=True)
+        status = os.waitpid(process.pid, 0)[1]
+        print("执行状态", status)
+
+
+if __name__ == '__main__':
+    _Helper._linux_cmd("ls")
